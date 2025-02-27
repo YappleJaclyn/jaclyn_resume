f6ca77659c6883d786625938474f9b35b56b9fdc
YappleJaclyn <jacklf@DESKTOP-3JJ4ALU>
2025-02-27 13:59:34 -0500

    jaclyned

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'yapplejack', // Usually your GitHub org/user name.
+  organizationName: 'YappleJaclyn', // Usually your GitHub org/user name.
   projectName: 'jaclyn_resume', // Usually your repo name.
   trailingSlash: false,
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -45,7 +45,6 @@ export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Jack Ferguson`}
       description="Swiss Army Knife Engineer">
       <HomepageHeader />
       <Tabs className='tabs--block'>
